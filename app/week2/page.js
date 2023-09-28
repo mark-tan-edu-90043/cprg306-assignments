8bba133ae9f3dae8c7d47d194a4a25e226f43d8b
mark-tan-edu-90043 <mark.tan01@edu.sait.ca>
2023-09-28 18:12:51 -0500

    week 2

---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "../StudentInfo"
+
+export default function Page() {
+    return (
+      <>
+        <h1>My Shopping List</h1>
+        <StudentInfo />
+        <br></br>
+      </>
+    )
+  }
