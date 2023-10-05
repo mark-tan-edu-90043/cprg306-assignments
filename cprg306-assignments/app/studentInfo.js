c4c541a0308853c8f4d22cc77f81c6f26692b186
mark-tan-edu-90043 <mark.tan01@edu.sait.ca>
2023-10-05 18:14:53 -0500

    Should be okay

---
+++ cprg306-assignments/app/studentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <>
+      <p>Name: Mark</p>
+      <p>Course Section: CPRG-306 ?</p>
+      <Link href="https://github.com/mark-tan-edu-90043/cprg306-assignments">[Github Link]</Link>
+    </>
+  )
+}
---
+++ cprg306-assignments/app/week2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "../studentInfo";
+
+export default function page() {
+    return(
+        <div>
+            <h1 className="text-4xl">My Shopping List</h1>
+
+            <StudentInfo />
+        </div>
+    )
+}
