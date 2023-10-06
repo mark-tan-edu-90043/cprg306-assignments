c80eb7b6241a7b650d4452cb7c2136d81a1ac0f9
mark-tan-edu-90043 <mark.tan01@edu.sait.ca>
2023-10-05 19:22:57 -0500

    Week 3

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -9,6 +9,8 @@ export default function Home() {
       <StudentInfo></StudentInfo>
       <br />
       <Link href="/week2">Week 2</Link>
+      <br />
+      <Link href="/week3">Week 3</Link>
     </div>
   )
 }
